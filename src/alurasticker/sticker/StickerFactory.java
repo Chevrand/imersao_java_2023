74e46df4300b53e4676da46952b415f6e65de89e
Wanderson Chevrand <wandersonchevrand27@gmail.com>
2023-03-30 00:58:55 -0300

    Implementação da classe responsável pela criação de stickers

---
+++ src/alurasticker/sticker/StickerFactory.java
@@ -0,0 +1,70 @@
+package alurasticker.sticker;
+
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.FontMetrics;
+import java.awt.Graphics2D;
+import java.awt.Shape;
+import java.awt.font.FontRenderContext;
+import java.awt.font.TextLayout;
+import java.awt.geom.AffineTransform;
+import java.awt.geom.Rectangle2D;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+
+import javax.imageio.ImageIO;
+
+public class StickerFactory {
+	
+	public void createSticker(InputStream image, String stickerName) throws IOException {
+		// leitura da imagem
+		// InputStream localImage = new FileInputStream(new File("assets/movie.jpg"));
+		BufferedImage originalImage = ImageIO.read(image);
+		
+		// cria nova imagem com transparência e com novo tamanho (em memória)
+		int width = originalImage.getWidth();
+		int height = originalImage.getHeight();
+		int textBoxHeight = (height / 100) * 20;
+		int newHeight = height + textBoxHeight;
+		BufferedImage newImage = new BufferedImage(width, newHeight, BufferedImage.TRANSLUCENT);
+		
+		// copia a imagem original para a nova imagem (em memória)
+		Graphics2D graphics = (Graphics2D) newImage.getGraphics();
+		graphics.drawImage(originalImage,0, 0, null);
+		
+		// configura texto a ser escrito
+		String text = "Top 10 IMDB";
+		int textSize = (newHeight / 100) * 6;
+		Font font = new Font("impact", Font.BOLD, textSize);
+		graphics.setColor(Color.YELLOW);		
+		graphics.setFont(font);
+
+		FontMetrics fontMetrics = graphics.getFontMetrics();
+		Rectangle2D rectangle = fontMetrics.getStringBounds(text, graphics);
+		int textWidth = (int) rectangle.getWidth();
+		int textHeight = (int) rectangle.getHeight();
+		int textXPosition = (width - textWidth) / 2;
+		int textYPosition = (textBoxHeight - textHeight) / 2;
+
+		// escrever um texto personalizado na imagem (em memória)
+		graphics.drawString(text, textXPosition, newHeight - textYPosition);
+		
+		FontRenderContext fontRenderContext = graphics.getFontRenderContext();
+		TextLayout textLayout = new TextLayout(text, font, fontRenderContext);
+		Shape outline = textLayout.getOutline(null);
+		AffineTransform transform = graphics.getTransform();
+		transform.translate(textXPosition, (double) newHeight - textYPosition);
+		graphics.setTransform(transform);
+		BasicStroke outlineStroke = new BasicStroke(width * 0.004f);
+		graphics.setStroke(outlineStroke);
+		graphics.setColor(Color.BLACK);
+		graphics.draw(outline);
+		graphics.setClip(outline);
+		
+		// escrever a nova imagem em um arquivo local
+		ImageIO.write(newImage, "png", new File(String.format("stickers/%s.png", stickerName.replace(":", ""))));
+	}
+}
--- src/alurastickers/main/App.java
+++ src/alurastickers/main/App.java
@@ -1,6 +1,8 @@
 package alurastickers.main;
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.URI;
+import java.net.URL;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
@@ -8,21 +10,24 @@ import java.net.http.HttpResponse.BodyHandlers;
 import java.util.List;
 import java.util.Map;
 
+import alurasticker.sticker.StickerFactory;
 import alurastickers.parser.JsonParser;
 
 public class App {
 	
-	private static final String URL =
+	private static final String TOP_MOVIES_URL =
 			"https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/TopMovies.json";
 
 	private static final String MOVIE_TEMPLATE = "\nFilme %d\nTitulo: %s\nPoster: %s\nIMDB Rating: %s %s";
 
 	private static final String STAR = "✰";
+	
+	private static StickerFactory stickerFactory = new StickerFactory();
 
 	public static void main(String[] args) throws IOException, InterruptedException {
 		
 		// Criação da conexão HTTP para buscar os Top 10 filmes
-		URI uri = URI.create(URL);
+		URI uri = URI.create(TOP_MOVIES_URL);
 		HttpClient client = HttpClient.newHttpClient();
 		HttpRequest request = HttpRequest.newBuilder(uri).GET().build();		
 		HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
@@ -35,12 +40,22 @@ public class App {
 		// Exibição dos dados
 		listaFilmes.forEach(filme -> {
 			int i = listaFilmes.indexOf(filme) + 1;
+			String title = filme.get("title");
+			String imageUrl = filme.get("image");
+			String imdbRating = filme.get("imDbRating");
+
 			System.out.println(String.format(MOVIE_TEMPLATE
 					, i
-					, filme.get("title")
-					, filme.get("image")
-					, filme.get("imDbRating")
-					, printStarByRating(filme.get("imDbRating"))));
+					, title
+					, imageUrl
+					, imdbRating
+					, printStarByRating(imdbRating)));
+			try {
+				InputStream image = new URL(imageUrl).openStream();
+				stickerFactory.createSticker(image, title);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
 		});
 	}
 	
